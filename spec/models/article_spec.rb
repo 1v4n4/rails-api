811d605086de68502fdb2d7a415fdaeae2a91d21
1v4n4 <ivananl@yahoo.com>
2022-09-24 19:46:15 +0200

    Add article validation tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it "creates an article" do
-    # article = Article.create({title: 'Article title', content: 'Article content'})
-    # article = FactoryBot.create(:article)
-    article = create(:article)
-    expect(article.title).to eq('Article title')
+  describe '#validations' do
+    let(:article) { build(:article) }
+
+    it "creates a valid article" do
+      # article = Article.create({title: 'Article title', content: 'Article content'})
+      # article = FactoryBot.create(:article)
+      # article = build(:article) # no need to save an article
+
+      expect(article).to be_valid # article.valid == true
+    end
+
+    it "has an invalid title" do
+      article = build(:article, title: "")
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("Can't be blank")
+    end
   end
 end
