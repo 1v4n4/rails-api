998587ddc456ecdf815f51cad2e067d1a7d9bdd4
1v4n4 <ivananl@yahoo.com>
2022-09-25 00:38:49 +0200

    Add articles#show route test and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # get '/articles', to: 'articles#index'
-  resources :articles, only: [:index]
+  # get '/articles/:id', to: 'articles#show'
+  resources :articles, only: [:index, :show]
 end
--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe '/article routes' do
     expect(get '/articles').to route_to('articles#index')
     # expect(get '/articles?page=3').to route_to('article#index', page: {number: 3 })
   end
+
+  it 'routes to articles#show' do
+    expect(get 'articles/1').to route_to('articles#show', id: '1')
+  end
+
 end
