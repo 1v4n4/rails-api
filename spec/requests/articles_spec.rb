12350d567d4414ff33264aff1eb310ffc2c4326a
1v4n4 <ivananl@yahoo.com>
2022-09-25 21:07:00 +0200

    Add more request expectations

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -11,18 +11,30 @@ RSpec.describe ArticlesController do
   it 'returns a proper json' do
     article = create(:article)
     get '/articles'
-    expect(json_data).to eq(
-      [{
-        id: article.id.to_s,
-        type: 'article',
-        attributes: {
-          title: article.title,
-          content: article.content,
-          slug: article.slug
-        }
-      }
-    ]
-    )
+    # body = JSON.parse(response.body).deep_symbolize_keys
+    # expect(body).to eq(
+    #   data: [{
+    #     id: article.id.to_s,
+    #     type: 'article',
+    #     attributes: {
+    #       title: article.title,
+    #       content: article.content,
+    #       slug: article.slug
+    #     }
+    #   }
+    # ]
+    # )
+    aggregate_failures do
+      expect(json_data.length).to eq(1)
+      expected = json_data.first
+      expect(expected[:id]).to eq(article.id.to_s)
+      expect(expected[:type]).to eq('article')
+      expect(expected[:attributes]).to eq({
+              title: article.title,
+              content: article.content,
+              slug: article.slug
+            })
+    end
   end
  end
 end
